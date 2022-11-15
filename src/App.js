d391401dfcdec61e23a3ecaba57ee988161c8ad6
Your Name <you@example.com>
2022-11-15 09:01:37 -0500

    feedback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+ const App = ()=>{
+
+    const bootcamps = [
+        {
+            id:1,
+            name:'Php bootcamp',
+            average_rating: 8
+        },
+        {
+            id:2,
+            name:'Js bootcamp',
+            average_rating: 10
+        }
+    ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Listado de bootcamps</h1>
+        <ul>
+            { bootcamps.map((bootcamp, index) => (
+                <li key={index}>{ bootcamp.name }</li>
+            )) }
+        </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
